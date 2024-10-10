fd28fd65c7aa4d247c4d07bcf6eb8062376fd376
Camilo Silva <117545916+CamiloS28@users.noreply.github.com>
2024-10-10 01:09:16 -0500

    CRUD EMPRESA Y DIVISA

---
+++ src/main/java/co/edu/unbosque/model/Empresa.java
@@ -0,0 +1,90 @@
+package co.edu.unbosque.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer empresa_id;
+
+    private String nombre;
+
+    private String sector;
+
+    private String pais;
+
+    private Double valor_mercado;
+
+    @ManyToOne
+    @JoinColumn(name = "divisa_id", nullable = false)
+    private Divisa divisa;
+
+    public Empresa() {
+    }
+
+    public Empresa(String nombre, String sector, String pais, Double valor_mercado, Divisa divisa) {
+        this.nombre = nombre;
+        this.sector = sector;
+        this.pais = pais;
+        this.valor_mercado = valor_mercado;
+        this.divisa = divisa;
+    }
+
+    // Getters y Setters
+    public Integer getEmpresa_id() {
+        return empresa_id;
+    }
+
+    public void setEmpresa_id(Integer empresa_id) {
+        this.empresa_id = empresa_id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public Double getValor_mercado() {
+        return valor_mercado;
+    }
+
+    public void setValor_mercado(Double valor_mercado) {
+        this.valor_mercado = valor_mercado;
+    }
+
+    public Divisa getDivisa() {
+        return divisa;
+    }
+
+    public void setDivisa(Divisa divisa) {
+        this.divisa = divisa;
+    }
+}
---
+++ src/main/java/co/edu/unbosque/repository/DivisaRepository.java
@@ -0,0 +1,14 @@
+package co.edu.unbosque.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import co.edu.unbosque.model.Divisa;
+
+public interface DivisaRepository extends JpaRepository<Divisa, Integer> {
+
+    public Optional<Divisa> findById(Integer id);
+
+    public List<Divisa> findAll();
+
+}
